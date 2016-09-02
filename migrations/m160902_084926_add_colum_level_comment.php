c1a634c1e6eb80a5d03d3f0e8f6cb59bcea08096
dumuro <dumuro@gmail.com>
2016-09-02 18:34:41 +0300

    +

--- migrations/m160902_084926_add_colum_level_comment.php
+++ migrations/m160902_084926_add_colum_level_comment.php
@@ -9,7 +9,7 @@ class m160902_084926_add_colum_level_comment extends Migration
 {
     public function up()
     {
-        $this->addColumn('comments','level', 'INT(11) AFTER pid');
+        $this->addColumn('comments','level', 'INT(11) NOT NULL DEFAULT 1 AFTER pid');
     }
 
     public function down(){}
